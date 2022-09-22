b6df3dd60c631985aff45572bfbb444e498fec11
aysenlxv <aysenlxv@gmail.com>
2022-09-22 21:30:36 +0900

    удалил блок ИНАЧЕ. Изменил переменную ответ и на вывод

--- quiz.py
+++ quiz.py
@@ -2,6 +2,7 @@
 
 answer = input("When was the first known use of the word 'quiz'? ")
 if answer == "1781":
-    print("Correct!")
-else:
-    print(f"The answer is '1781', not {answer!r}")
+    print("Wrong!")
+
+answer = "pizdets"
+print(answer)
