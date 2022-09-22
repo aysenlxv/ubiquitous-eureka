82a79c16d77c44bd514397d45e1c0323a14819b3
aysenlxv <aysenlxv@gmail.com>
2022-09-22 21:42:07 +0900

    вернули блок иначе и удалили pizdets upd: testspamegg

--- quiz.py
+++ quiz.py
@@ -2,7 +2,6 @@
 
 answer = input("When was the first known use of the word 'quiz'? ")
 if answer == "1781":
-    print("Wrong!")
-
-answer = "pizdets"
-print(answer)
+    print("Correct!")
+else:
+    print(f"The answer is '1781', not {answer!r}")
