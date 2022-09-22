c5f6c18023037b48140b90b8fbee9e81ad6df047
aysenlxv <aysenlxv@gmail.com>
2022-09-22 23:03:24 +0900

    удалил восклицательные знаки

--- quiz.py
+++ quiz.py
@@ -2,6 +2,6 @@
 
 answer = input("When was the first known use of the word 'quiz'? ")
 if answer == "1781":
-    print("Correct!!!")
+    print("Correct")
 else:
     print(f"The answer is '1781', not {answer!r}")
