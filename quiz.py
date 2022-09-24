87bc82e9d7f3c176e817402f92a9867cddfc7271
aysenlxv <aysenlxv@gmail.com>
2022-09-24 11:37:22 +0900

    удалили проверку правильного ответа

--- quiz.py
+++ quiz.py
@@ -2,9 +2,7 @@
 import mod
 
 answer = input("When was the first known use of the word 'quiz'? ")
-if answer == "1781":
-    print("Correct")
-else:
-    print(f"The answer is '1781', not {answer!r}")
+
+print(f"The answer is '1781', not {answer!r}")
 
 print(mod.var)
