651ffdbf51554d483153aaaacdc5cb11a1855d1f
aysenlxv <aysenlxv@gmail.com>
2022-09-24 11:34:47 +0900

    Изменил верный ответ и то, что выводит при правильном ответе

--- quiz.py
+++ quiz.py
@@ -2,9 +2,9 @@
 import mod
 
 answer = input("When was the first known use of the word 'quiz'? ")
-if answer == "1781":
-    print("Correct")
+if answer == "17818":
+    print("Right")
 else:
-    print(f"The answer is '1781', not {answer!r}")
+    print(f"The answer is '17818', not {answer!r}")
 
 print(mod.var)
